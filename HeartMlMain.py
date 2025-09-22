48a89932715a1a7c4411cc2a956c7583509f1450
Callum Poole <CallumPoole@protonmail.com>
2025-09-22 17:04:44 +0100

    First Commit (Random Forest Classifier)

---
+++ HeartMlMain.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve, roc_auc_score
+
+# print(f"Pandas - {pd.__version__}, NumPy - {np.__version__}, Sklearn - {sklearn.__version__}")
+
+def import_data(X_train_path, y_train_path):
+    X_train = pd.read_csv(X_train_path)
+    y_train = pd.read_csv(y_train_path).squeeze()
+    return X_train, y_train
+
+def train_model(X_train, y_train):
+    clf = RandomForestClassifier(class_weight='balanced', random_state=42)
+    clf.fit(X_train, y_train)
+    return clf
+
+def evaluate_model(clf, X_val, y_val):
+    val_acc = clf.score(X=X_val, y=y_val)
+    print(f"The model's accuracy on the test set is: {val_acc*100}%")
+
+    y_probs = clf.predict_proba(X_val)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_val, y_probs, pos_label=1)
+    auc = roc_auc_score(y_val, y_probs)
+
+    plt.plot(fpr, tpr, label=f"AUC = {auc:.2f}")
+    plt.plot([0,1], [0,1], "k--")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend()
+    plt.show()
+
+def main():
+    X_train, y_train = import_data("X_train.csv", "y_train.csv")
+    
+    X_train_new, X_val, y_train_new, y_val = train_test_split(
+        X_train, y_train, test_size=0.25, random_state=42, stratify=y_train
+    )
+    
+    clf = train_model(X_train_new, y_train_new)
+    
+    evaluate_model(clf, X_val, y_val)
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ train-test-split.py
@@ -0,0 +1,28 @@
+# import pandas as pd
+# import numpy as npls
+# import matplotlib.pyplot as plt
+# import sklearn
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+
+# print(pd.__version__)
+# print(np.__version__)
+# print(plt.__version__)
+# print(sklearn.__version__)
+
+heart_raw = pd.read_csv('heart_disease_health_indicators_BRFSS2015.csv')
+# print(heart_raw.head())
+
+X = heart_raw.drop("HeartDiseaseorAttack", axis=1)
+
+# Create y (the target column)
+y = heart_raw["HeartDiseaseorAttack"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, stratify=y
+)
+
+X_train.to_csv("X_train.csv", index=False)
+y_train.to_csv("y_train.csv", index=False)
+X_test.to_csv("X_test.csv", index=False)
+y_test.to_csv("y_test.csv", index=False)
