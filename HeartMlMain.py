75e4c19ab5b3f2d23949becef8c1510a82abb4d7
Callum Poole <CallumPoole@protonmail.com>
2025-09-25 10:51:02 +0100

    Adding timer to asses performance and adding comments

--- HeartMlMain.py
+++ HeartMlMain.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
+import time
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_curve, roc_auc_score
@@ -12,16 +13,19 @@ from sklearn.pipeline import Pipeline
 
 # print(f"Pandas - {pd.__version__}, NumPy - {np.__version__}, Sklearn - {sklearn.__version__}")
 
+# Importing data from train-test-split.py. Contains already split into X and y training data
 def import_data(X_train_path, y_train_path):
     X_train = pd.read_csv(X_train_path)
     y_train = pd.read_csv(y_train_path).squeeze()
     return X_train, y_train
 
+# training the model using RandomForestClassifier on the train set
 def train_model(X_train, y_train):
     clf = RandomForestClassifier(class_weight='balanced', random_state=42)
     clf.fit(X_train, y_train)
     return clf
 
+# evaluating the model on the validation set andplotting the ROC curve
 def evaluate_model(clf, X_val, y_val):
     val_acc = clf.score(X=X_val, y=y_val)
     print(f"The model's accuracy on the test set is: {val_acc*100}%")
@@ -36,7 +40,7 @@ def evaluate_model(clf, X_val, y_val):
     plt.ylabel("True Positive Rate")
     plt.title("ROC Curve")
     plt.legend()
-    plt.show()
+   
 
 def main():
     X_train, y_train = import_data("X_train.csv", "y_train.csv")
@@ -51,7 +55,11 @@ def main():
 
 
 if __name__ == "__main__":
+    # Measure execution time
+    start = time.perf_counter()
     main()
+    end = time.perf_counter()
+    print(f"Execution time: {end - start:.4f} seconds")
 
 
     
