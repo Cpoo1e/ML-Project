aaacd05424b89edc448fa7613e5a46d637abb90e
Callum Poole <CallumPoole@protonmail.com>
2025-09-28 14:15:46 +0100

    Added Sk pipeline and logistic regression

--- HeartMlMain.py
+++ HeartMlMain.py
@@ -5,6 +5,9 @@ import sklearn
 import time
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.compose import ColumnTransformer
 from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
@@ -21,10 +24,27 @@ def import_data(X_train_path, y_train_path):
 
 # training the model using RandomForestClassifier on the train set
 def train_model(X_train, y_train):
-    clf = RandomForestClassifier(class_weight='balanced', random_state=42)
+    # clf = RandomForestClassifier(class_weight='balanced', random_state=42)
+    # clf.fit(X_train, y_train)
+    # return clf
+
+    continuous_features = ['BMI', 'Age', 'MentHlth', 'PhysHlth', 'GenHlth', 'Diabetes', 'Education', 'Income']
+    categorical_features = [col for col in X_train.columns if col not in continuous_features + ['HeartDiseaseorAttack']]
+
+    prepro = ColumnTransformer(
+        transformers=[
+            ('num', StandardScaler(), continuous_features),
+            ('cat', 'passthrough', categorical_features)    
+        ])
+
+    clf = Pipeline(steps=[
+        ('preprocessor', prepro),
+        ('classifier', LogisticRegression(class_weight='balanced', random_state=42))
+    ])
+
     clf.fit(X_train, y_train)
     return clf
-
+    
 # evaluating the model on the validation set andplotting the ROC curve
 def evaluate_model(clf, X_val, y_val):
     val_acc = clf.score(X=X_val, y=y_val)
@@ -40,6 +60,7 @@ def evaluate_model(clf, X_val, y_val):
     plt.ylabel("True Positive Rate")
     plt.title("ROC Curve")
     plt.legend()
+    plt.show()
    
 
 def main():
@@ -60,9 +81,3 @@ if __name__ == "__main__":
     main()
     end = time.perf_counter()
     print(f"Execution time: {end - start:.4f} seconds")
-
-
-    
-    # clf = train_model(X_train_new, y_train_new)
-    
-    # evaluate_model(clf, X_val, y_val)
